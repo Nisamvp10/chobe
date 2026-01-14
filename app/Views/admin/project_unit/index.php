32eeedcd00c0cfb6cf266df80654afd5ee2a1974
Nisam Git <nisamvp10@gmail.com>
2026-01-14 15:34:27 +0530

    project unit filter hide

--- app/Views/admin/project_unit/index.php
+++ app/Views/admin/project_unit/index.php
@@ -43,7 +43,7 @@
             </div>
             
             <!-- Column 2: Status Dropdown -->
-            <div class="w-full md:w-48">
+            <div class="w-full md:w-48 hidden">
                 <div class="relative">
                     <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-filter text-gray-400">
