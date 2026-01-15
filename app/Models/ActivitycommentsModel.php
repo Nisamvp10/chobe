e9aadd45d9aaabb460a8781f6735c3218d2f0210
Nisam Git <nisamvp10@gmail.com>
2026-01-15 10:12:16 +0530

    msal activitycomment

--- app/Models/ActivitycommentsModel.php
+++ app/Models/ActivitycommentsModel.php
@@ -8,13 +8,16 @@ class ActivitycommentsModel extends Model{
     protected $allowedFields = ['id','task_id','activity_id','user_id','comment','status','created_by','created_at'];
     protected $primaryKey = 'id';
 
-    public function allComments($taskId=false,$activityId) {
+    public function allComments($taskId=false,$activityId =false) {
         $builder = $this->db->table('activities_comments as ac')
                     ->select('ac.id,ac.created_at,ac.created_by,ac.comment,
                     u.name')
                     ->join('users as u','u.id= ac.user_id')
-                    ->where(['ac.task_id' => $taskId,'ac.activity_id'=>$activityId])
-                    ->orderBy('ac.id ASC');
+                    ->where(['ac.task_id' => $taskId]);
+                    if($activityId) {
+                        $builder->where('ac.activity_id',$activityId);
+                    }
+                    $builder->orderBy('ac.id ASC');
         $result = $builder->get()->getResultArray();
         return $result;
     }
