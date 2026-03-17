1ea1705ce4f56bbea7b74049d33fb9d946b503c1
Nisam Git <nisamvp10@gmail.com>
2026-03-17 16:51:42 +0530

    project unit name done in activities task

--- app/Services/Common.php
+++ app/Services/Common.php
@@ -10,7 +10,7 @@ class Common {
     
     public function getBranchNameBytaskId($taskId) {
         $builder = $this->taskModel->select('tasks.id, tasks.title,tasks.task_gen_date, project_unit.store, project_unit.oldstore_name, project_unit.oracle_code, project_unit.polaris_code')
-        ->join('project_unit', 'project_unit.id = tasks.project_id')
+        ->join('project_unit', 'project_unit.id = tasks.project_unit')
         ->where('tasks.id', $taskId);
         $result = $builder->get()->getRow();
         return $result;
